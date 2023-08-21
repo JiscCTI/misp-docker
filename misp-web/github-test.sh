a870a9fe21ca961762e6f152d637b8053f781b36
Joe Pitt <joe.pitt@jisc.ac.uk>
2023-08-21 11:57:21 +0100

    fix: test using service name
    github-action-docker-compose-test-run runs test in a new container

--- misp-web/github-test.sh
+++ misp-web/github-test.sh
@@ -8,19 +8,19 @@
 echo "Waiting 15 minutes for initial startup to finish..."
 sleep 900
 
-for i in $(seq 1 120)
+for i in $(seq 1 90)
 do
-    echo "$i/120: Testing if MISP is up..."
-    if curl -fkq https://127.0.0.1/users/login ; then
-        echo "$i/120: MISP is up, test completed successfully."
+    echo "$i/90: Testing if MISP is up..."
+    if curl -fk https://misp_web/users/login ; then
+        echo "$i/90: MISP is up, test completed successfully."
         exit 
     else
-        if [ "$i" -eq 120 ]; then
-            echo "$i/120: MISP isn't up after 25 minutes, test failed."
+        if [ "$i" -eq 1200 ]; then
+            echo "$i/90: MISP isn't up after 45 minutes, test failed."
             exit 1
         else
-            echo "$i/120: MISP isn't up yet, waiting 5 seconds..."
-            sleep 5
+            echo "$i/90: MISP isn't up yet, waiting 30 seconds..."
+            sleep 30
         fi
     fi
 done

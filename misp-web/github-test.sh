46a638e25d17353846a3beeacc105d3d0d0f32e3
Joe Pitt <joe.pitt@jisc.ac.uk>
2023-08-21 08:57:35 +0100

    fix: test not iterating

--- misp-web/github-test.sh
+++ misp-web/github-test.sh
@@ -5,12 +5,13 @@
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
-for i in {1..300}
-do
-    if curl -fk https://127.0.0.1/users/login; then
+for ((i=1;i<=300;i++)); do
+    if curl -fk https://127.0.0.1/users/login ; then
+        echo "MISP is up"
         exit 
     else
+        echo "MISP isn't up yet, waitng 3 seconds..."
         sleep 3
     fi
 done
-
+exit 1

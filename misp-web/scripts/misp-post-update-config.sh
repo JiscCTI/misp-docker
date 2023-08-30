c04d513466f75a9a4b879de638a8a305040dc958
Joe Pitt <joe.pitt@jisc.ac.uk>
2023-08-30 17:58:31 +0100

    fix: diagnostics page crash new in v2.4.175
    related-to: https://github.com/MISP/MISP/pull/9230

--- misp-web/scripts/misp-post-update-config.sh
+++ misp-web/scripts/misp-post-update-config.sh
@@ -10,3 +10,7 @@ $CAKE Admin setSetting "MISP.attachment_scan_module" "clamav"
 $CAKE Admin setSetting "MISP.correlation_engine" "Default"
 $CAKE Admin setSetting "MISP.log_new_audit_compress" true
 $CAKE Admin setSetting "Security.otp_required" true
+$CAKE Admin setSetting "MISP.self_update" false
+
+# Workaround diagnostics page issue introduced in v2.4.175 (https://github.com/MISP/MISP/pull/9230)
+$CAKE Admin setSetting "MISP.online_version_check" false

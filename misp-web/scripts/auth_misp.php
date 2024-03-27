151c1c992ac6f729875c38c5e6360e2f914cd152
Joe Pitt <joe.pitt@jisc.ac.uk>
2024-03-27 12:26:08 +0000

    feat: allow switching auth methods

--- misp-web/scripts/auth_misp.php
+++ misp-web/scripts/auth_misp.php
@@ -3,11 +3,19 @@ include '/var/www/MISPData/config/config.php';
 
 unset($config["Security"]["auth"]);
 unset($config["ApacheShibbAuth"]);
-
+unset($config["OidcAuth"]);
 file_put_contents("/var/www/MISPData/config/config.php", "<?php\n\$config = " . var_export($config, true) . ";");
 
 $bootstrap = file_get_contents("/var/www/MISPData/config/bootstrap.php");
+$bootstrap_changed = false;
 if (strpos($bootstrap, "CakePlugin::load('ShibbAuth');") !== false) {
     $bootstrap = str_replace("\nCakePlugin::load('ShibbAuth');\n", "", $bootstrap);
+    $bootstrap_changed = true;
+}
+if (strpos($bootstrap, "CakePlugin::load('OidcAuth');") !== false) {
+    $bootstrap = str_replace("\nCakePlugin::load('OidcAuth');\n", "", $bootstrap);
+    $bootstrap_changed = true;
+}
+if ($bootstrap_changed) {
     file_put_contents("/var/www/MISPData/config/bootstrap.php", $bootstrap);
 }

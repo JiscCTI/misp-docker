cc0edbcff42ce4d1d6c7ea87a7dfdc464663eb8d
Joe Pitt <joe.pitt@jisc.ac.uk>
2024-05-08 14:35:00 +0100

    docs: add missing REUSE headers

--- misp-shibb-sp/entrypoint.sh
+++ misp-shibb-sp/entrypoint.sh
@@ -4,6 +4,11 @@
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
+# SPDX-FileCopyrightText: 2024 Jisc Services Limited
+# SPDX-FileContributor: Joe Pitt
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
 set -e
 
 set_env_vars() {
